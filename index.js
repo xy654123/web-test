9816aaa901d6c6ff5a6ee1c7e69efef3062f0f54
xy654123 <96267331+xy654123@users.noreply.github.com>
2022-07-01 11:48:16 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -14,3 +14,17 @@ var imgTag = document.getElementById("photo");
 imgTag.setAttribute("src","img/도시"+num+".jpg");
 }
 
+let slideIndex = 5;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000);
+}
